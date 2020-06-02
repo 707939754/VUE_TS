7212ab7876f4e42ef13ae62bfd8940e59a1818c3
Chuanecey Li <707939754@qq.com>
2020-06-02 14:57:37 +0800

    cs

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -6,7 +6,7 @@ export default class Home extends Vue {
     public render() {
         return (
             <div>
-                home
+                homelist
             </div>
         )
     }
